7cf8377f48d80ce542ce7e9aaa6834c9d785f3f9
FelixTheC <felixeisenmenger@gmx.net>
2017-06-26 22:04:53 +0200

    [Add] excercise game hangmann

--- hangmann.py
+++ hangmann.py
@@ -3,6 +3,8 @@
 from random import choice
 
 def getAWord():
+    """the word list is from http://norvig.com/ngrams/sowpods.txt
+    """
     words = []
     with open('30_Pick_Word.txt', 'r') as f:
         for line in f:
@@ -13,13 +15,17 @@ def showHiddenWord(word):
     return ['_' for i in range(len(word))]
         
 
-def openHiddenField(word, hiddenliste, dic):
+def openHiddenField(hiddenliste, dic):
     for key in dic:
         hiddenliste[key] = dic[key]
     return hiddenliste
 
 
 def charInWord(char, word):
+    """checks if a char is in the word therefore I created a dic because it could be that
+    one char is more than one time in a word. 
+    The key is the index and the right guest char is the value
+    """
     dic = {}
     if char in word:
         for i in range(len(word)):
@@ -30,4 +36,37 @@ def charInWord(char, word):
         return False
     
     
-print(openHiddenField('narwal',showHiddenWord('narwal'),charInWord('a','narwal')))
+def createWordFromList(wordListe):
+    word = ''
+    return word.join(wordListe)
+
+
+def main():
+    fails = 0
+    print('Welcome to hangman')
+    theWord = getAWord()
+    hidden = showHiddenWord(theWord)
+    print(hidden)
+    while True:
+        user = input('Time for your guess ')
+        check = charInWord(user, theWord)
+        if check:
+            #everytime a guess is right the field turns into the char and will be shown
+            print(openHiddenField(hidden, check))
+            if '_' not in openHiddenField(hidden, check):
+                print('You win '+ createWordFromList(openHiddenField(hidden, check)) + ' is the correct answer')
+                break
+        else:
+            fails += 1
+        if fails > 5:
+            print('The correct answer is ' + theWord)
+            break
+    again = input('Do you want a new game y/n ')
+    if again.lower() == 'y':
+        main()
+    else:
+        print('Goodbye')
+            
+if __name__ == '__main__':
+    main()
+    
